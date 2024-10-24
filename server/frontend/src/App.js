36f7dcab0aab70b350c161934688c8327531d12e
PetWolowitz <156096630+PetWolowitz@users.noreply.github.com>
2024-10-24 09:39:35 +0200

    pullato fatto il merging e pushato

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,9 @@
 import LoginPanel from "./components/Login/Login";
 import Register from "./components/Register/Register";  // Importa il componente di registrazione
 import { Routes, Route } from "react-router-dom";
+import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer";
+import PostReview from "./components/Dealers/PostReview"
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Route path="/login" element={<LoginPanel />} />
       {/* Rotta per la registrazione */}
       <Route path="/register" element={<Register />} />
+      <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />   
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
