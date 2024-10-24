ff72c4ade38ce3824c0787c62d35bf40b763db04
PetWolowitz <156096630+PetWolowitz@users.noreply.github.com>
2024-10-24 09:34:02 +0200

    Salvo le modifiche locali prima di fare pull

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('logout/', views.logout_request, name='logout'),
     path('get_cars/', views.get_cars, name='get_cars'),
     # path for dealer reviews view
-    path('get_dealers', views.get_dealerships, name='get_dealers'),
-    path('get_dealers/<str:state>', views.get_dealerships, name='get_dealers_by_state'),
+   path('get_dealers/', views.get_dealerships, name='get_dealers'),
+    path('get_dealers/<str:state>/', views.get_dealerships, name='get_dealers_by_state'),
     # Route per ottenere le recensioni di un concessionario specifico
     path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews, name='dealer_details'),
         path(route='add_review', view=views.add_review, name='add_review'),
--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,28 +1,18 @@
-"""djangoproj URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+from djangoapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="Home.html"), name='home'),
+    path('login/', TemplateView.as_view(template_name="index.html")),  # Punto di ingresso per la pagina di login
+    path('register/', TemplateView.as_view(template_name="index.html")),  # Punto di ingresso per la pagina di registrazione
     path('about/', TemplateView.as_view(template_name="About.html"), name='about'),
     path('contact/', TemplateView.as_view(template_name="Contact.html"), name='contact'),
-    path('', include('djangoapp.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
+
+    
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  
